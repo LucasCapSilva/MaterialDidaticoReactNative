38d6a6d554e14a821e5edd8a4804e6c56ba26bca
LucasCapSilva <57732522+LucasCapSilva@users.noreply.github.com>
2020-08-20 22:31:57 -0300

    Add files via upload

---
+++ youtubes/src/page/Login/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+import { View, Image,TouchableOpacity } from 'react-native';
+import { TextInput,Text,Button,StyleSheet } from 'react-native';
+
+
+export default class Login extends Component {
+  static navigationOptions = ({ navigation }) => ({
+      title: 'Video'
+    });
+    render() {
+  return (
+    <View flex={1} style={{ justifyContent: "flex-start" }}>
+    <View>
+    <Text style={{fontSize: 28,
+fontWeight: "bold", paddingTop:10, alignSelf:"center"}}>Login</Text>
+    <Text style={{fontSize: 25,
+fontWeight: "bold", paddingTop:10, paddingLeft:10}}>Email</Text>
+    <TextInput
+   
+  style={{ height: 40, borderColor: 'gray',fontSize: 25, borderWidth: 1 }}
+/>
+      <Text style={{fontSize: 25,
+fontWeight: "bold", paddingTop:10, paddingLeft:10}}>Senha</Text>
+   <TextInput
+  
+  style={{ height: 40, borderColor: 'gray',fontSize: 25, borderWidth: 1 }}
+/>
+    </View>
+    <View style={{ alignItems: "flex-start",  flexDirection: 'row',
+justifyContent: 'space-between', paddingLeft:15, paddingTop:20 }}>
+            <Button
+             onPress={() => this.props.navigation.navigate('Home')}
+            title="Login"
+            color="green"
+          />
+     
+      <View style={{ flexDirection:"row", alignItems:"center" }} >
+       
+      </View>
+
+    </View>
+  </View>
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
