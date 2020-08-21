7d16ed220ffc4b397f1aeb4cf062ad488c93fc12
LucasCapSilva <57732522+LucasCapSilva@users.noreply.github.com>
2020-08-20 22:20:01 -0300

    Add files via upload

---
+++ stacknavs5/App.js
@@ -0,0 +1,25 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/page/home';
+import Login from './src/page/Login';
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
